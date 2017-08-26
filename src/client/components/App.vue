934a240a8ed23d41d0048d22844c0408390ee227
Demetrio <dmitr77@gmail.com>
2017-08-25 19:38:29 -0500

    Adding Vuex

--- src/client/components/App.vue
+++ src/client/components/App.vue
@@ -2,8 +2,24 @@
   <div>
     <h1>HI</h1>
     <Home></Home>
+    <p>{{ status }}</p>
+    <button @click="changeStatus">Change Status</button>
   </div>
 </template>
 
 <script>
+  export default {
+    data: function() {
+      return {
+        msg: 'data message',
+        status: 'In Progress'
+      }
+    },
+    methods: {
+      changeStatus() {
+        this.status = 'Done';
+      }
+    }
+  }
+
 </script>
--- src/client/components/main.js
+++ src/client/components/main.js
@@ -1,10 +1,13 @@
 /* eslint-disable*/
 import Vue from 'vue'
+import Vuex from 'vuex'
+
 //main component
 import App from './App.vue'
 //other components
 import Home from './Home.vue'
 
+Vue.use(Vuex)
 //register component to be used globally including inside other components
 Vue.component('Home', Home)
 
