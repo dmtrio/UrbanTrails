8b23640a85fc41c5dd075062152bed2c18a56cab
Demetrio <dmitr77@gmail.com>
2017-08-28 16:18:24 -0500

    Removed dead code from server

--- src/server/index.js
+++ src/server/index.js
@@ -2,15 +2,7 @@ const Hapi = require('hapi')
 const path = require('path')
 
 // Create a server with a host and port
-const server = new Hapi.Server({
-  // connections: {
-  //   routes: {
-  //     files: {
-  //       relativeTo: path.join(__dirname, '../client/')
-  //     }
-  //   }
-  // }
-})
+const server = new Hapi.Server()
 
 server.connection({
   host: 'localhost',
@@ -33,44 +25,13 @@ server.register(require('inert'), (err) => {
       }
     }
   })
-
-  // server.route({
-  //   method: 'GET',
-  //   path: '/',
-  //   handler: (request, reply) => //{
-  //     // console.log(path.join(__dirname))
-  //     reply.file('index.html'),
-  //     // }
-  // })
 })
-
-// server.route({
-//   method: 'GET',
-//   path: '/index.html',
-//   handler: (request, reply) => {
-//
-//     console.log('hit here')
-//     reply.file('./src/client/index.html')
-//   },
-// })
 
 server.route({
   method: 'GET',
   path: '/hello',
   handler: (request, reply) => reply('hello world'),
 })
-
-// server.route({
-//   method: 'GET',
-//   path: '/dist/build.js',
-//   handler: (request, reply) => reply.file('./src/client/dist/build.js'),
-// })
-
-// server.route({
-//   method: 'GET',
-//   path: '/manifest.json',
-//   handler: (request, reply) => reply.file('./src/client/manifest.json'),
-// })
 
 // Start the server
 server.start((err) => {
