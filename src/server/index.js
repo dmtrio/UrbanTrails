b9e2ac612ba72b66e03f99c5aad9573654cc27fa
Demetrio <dmitr77@gmail.com>
2017-08-28 16:16:43 -0500

    Hapi now serves all static file under client as requested. Progressive web app manifest added

--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,24 @@
 const Hapi = require('hapi')
+const path = require('path')
 
 // Create a server with a host and port
-const server = new Hapi.Server()
+const server = new Hapi.Server({
+  // connections: {
+  //   routes: {
+  //     files: {
+  //       relativeTo: path.join(__dirname, '../client/')
+  //     }
+  //   }
+  // }
+})
+
 server.connection({
   host: 'localhost',
   port: 8000,
 })
 
+
 // Add the route
-server.route({
-  method: 'GET',
-  path: '/hello',
-  handler: (request, reply) => reply('hello world'),
-})
-
-
 server.register(require('inert'), (err) => {
   if (err) {
     throw err
@@ -22,16 +26,51 @@ server.register(require('inert'), (err) => {
 
   server.route({
     method: 'GET',
-    path: '/',
-    handler: (request, reply) => reply.file('./src/client/index.html'),
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: path.join(__dirname, '../client/')
+      }
+    }
   })
+
+  // server.route({
+  //   method: 'GET',
+  //   path: '/',
+  //   handler: (request, reply) => //{
+  //     // console.log(path.join(__dirname))
+  //     reply.file('index.html'),
+  //     // }
+  // })
 })
+
+// server.route({
+//   method: 'GET',
+//   path: '/index.html',
+//   handler: (request, reply) => {
+//
+//     console.log('hit here')
+//     reply.file('./src/client/index.html')
+//   },
+// })
 
 server.route({
   method: 'GET',
-  path: '/dist/build.js',
-  handler: (request, reply) => reply.file('./src/client/dist/build.js'),
+  path: '/hello',
+  handler: (request, reply) => reply('hello world'),
 })
+
+// server.route({
+//   method: 'GET',
+//   path: '/dist/build.js',
+//   handler: (request, reply) => reply.file('./src/client/dist/build.js'),
+// })
+
+// server.route({
+//   method: 'GET',
+//   path: '/manifest.json',
+//   handler: (request, reply) => reply.file('./src/client/manifest.json'),
+// })
 
 // Start the server
 server.start((err) => {
