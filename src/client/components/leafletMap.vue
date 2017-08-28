3a0705cf0f78dcbd0534f96ddb72bbc8462f37fa
Timothy Roy <roy.tim@gmail.com>
2017-08-28 18:43:45 -0500

    Adds GeoJSON trail data to a leaflet map
    Conflicts:
    	.gitignore
    	package.json
    	src/client/components/leafletMap.vue

---
+++ src/client/components/leafletMap.vue
@@ -0,0 +1,65 @@
+<template>
+ <div id="mapid"></div>
+</template>
+
+<script>
+
+import * as L from 'leaflet';
+import fixit from './fixit.json'
+import AustinTrails from './AustinTrails.geojson'
+
+export default {
+  data() {
+    return {
+      data: fixit.data,
+      w: 700,
+      h: 580
+    };
+  },
+  mounted() {
+    this.makeMap();
+  },
+  methods: {
+    makeMap() {
+    console.log(this.$data.data[0][11])
+      let mymap = L.map('mapid').locate({setView: true, maxZoom: 14});
+      
+      L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.dark/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidGlyb3kiLCJhIjoiY2o2d21xbHRiMXhqOTJ3bGFxZ3l2bm1sMSJ9.rIS4v4TvYEdQctZulEKzCg', {
+      maxZoom: 18,
+      id: 'mapbox.streets'
+      }).addTo(mymap);
+
+      this.$data.data.forEach((chunk) => {
+      let lat = chunk[11][1]
+      let lon = chunk[11][2]
+      let name = chunk[8]
+      let address = JSON.parse(chunk[11][0]).address
+      let marker = L.marker([lat, lon]).addTo(mymap);
+      marker.bindPopup(`<b>${name}</b><br>${address}`)
+      })
+
+      L.geoJSON(AustinTrails).addTo(mymap);
+
+      function onLocationFound(e) {
+          var radius = e.accuracy / 2;
+
+              L.marker(e.latlng).addTo(mymap)
+                      .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+                          L.circle(e.latlng, radius).addTo(mymap);
+                          }
+
+                          mymap.on('locationfound', onLocationFound);
+
+    },
+    
+  },
+};
+
+
+</script>
+
+<style>
+#mapid { height: 500px; }
+
+</style>
