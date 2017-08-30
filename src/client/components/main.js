04598f55b245beab3b7e1d1893905cc0c125f7a8
ceciliamvrie <cecilia.mvrie@gmail.com>
2017-08-30 15:51:12 -0500

    added vuex for storing map data

--- src/client/components/main.js
+++ src/client/components/main.js
@@ -1,45 +1,26 @@
-/* eslint-disable*/
 import Vue from 'vue'
-import Vuex from 'vuex'
 import * as Vue2Leaflet from 'vue2-leaflet'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
+import VueAsyncData from 'vue-async-data'
+//vuex store
+import store from '../vuex/store'
 //main component
 import App from './App.vue'
 //other components
-import Home from './Home.vue'
 import Map from './Map.vue'
 import leafletMap from './leafletMap.vue'
 
-//VUEAXIOS LIBRARY FOR INTEGRATING VUE WITH AXIOS
-Vue.use(VueAxios, axios)
-
-//VUEX STORE FOR ALL COMPONENTS
-Vue.use(Vuex)
-const store = new Vuex.Store({
-  state: {
-    count: 0
-  },
-  mutations: {
-    increment (state) {
-      state.count++
-    }
-  }
-})
+Vue.use(VueAsyncData)
 
 //register component to be used globally including inside other components
-Vue.component('Home', Home)
 Vue.component('Map', Map)
 Vue.component('leafletMap', leafletMap)
 Vue.component('v-map', Vue2Leaflet.Map);
 Vue.component('v-tilelayer', Vue2Leaflet.TileLayer);
 Vue.component('v-marker', Vue2Leaflet.Marker);
-Vue.component('leafletMap', leafletMap)
 
 new Vue({
-  // Passing down Vuex store to all child components
-  store,
+  //inject store into all components
+  store: store,
   //element where it will be rendered
   el: '#app',
   //render app component to the dom
