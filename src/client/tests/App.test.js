6019b4eaee92a9b0c9ef759da96f6b8dbdc7cb73
Timothy Roy <roy.tim@gmail.com>
2017-08-30 19:14:40 -0500

    Fixes merge conflict and test

--- src/client/tests/App.test.js
+++ src/client/tests/App.test.js
@@ -4,12 +4,6 @@ import App from '../components/App.vue'
 
 // Inspect the raw component options
 test('App has a computed hook', () => {
-  expect(App.computed.count).toBeInstanceOf(Function)
-})
-
-// inspect default data
-test('App', () => {
-  const defaultData = App.data()
-  expect(defaultData.status).toBe('In Progress')
+  expect(App.mounted).toBeInstanceOf(Function)
 })
 
