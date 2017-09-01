0d436b3694a410c03de7e6c1951045839ad98e8b
Demetrio <dmitr77@gmail.com>
2017-09-01 11:57:05 -0500

    Linter fixes

--- src/client/components/leafletMethods/leafletMethods.js
+++ src/client/components/leafletMethods/leafletMethods.js
@@ -1,29 +1,3 @@
 export default {
-  drug: () => console.log('dosed'),
-  fixitMarkers: (context) => {
-    context.fixits.forEach((chunk) => {
-      let lat = chunk[11][1]
-      let lon = chunk[11][2]
-      let name = chunk[8]
-      let address = JSON.parse(chunk[11][0]).address
-      let marker = L.marker([lat, lon])
-      marker.bindPopup(`<b>${name} Fixit Station</b><br>${address}`)
-      context.$data.fixitsLayer.addLayer(marker)
-    })
-  },
-  kioskMarkers(context) {
-    context.kiosks.forEach((chunk) => {
-      if (chunk[10] === 'active') {
-        let address = chunk[9]
-        let lat = chunk[11]
-        let lon = chunk[12]
-        let marker = L.marker([lat, lon])
-        marker.bindPopup(`${address} Bicycle Kiosk`)
-        context.$data.kiosksLayer.addLayer(marker)
-      }
-    })
-  },
-  addTrails(context) {
-    context.$data.trailsLayer.addData(map.trails)
-  },
+  drug: () => console.log('dosed')
 }
