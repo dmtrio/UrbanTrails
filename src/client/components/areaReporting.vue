04b6751c9d713ee50c19fd471fd9776cc7510ecc
Andrew Marshall <Minkzx@users.noreply.github.com>
2017-09-01 17:21:13 -0500

    made central css file, added beginnings of reporting

--- src/client/components/areaReporting.vue
+++ src/client/components/areaReporting.vue
@@ -1,18 +1,36 @@
 <template>
   <div>
     <div class="reporting">
-      <!--Find coordinates of click-->
-      <!--Set marker at click coordinates-->
-      <!--Determine whether report is an issue or recommendation-->
+      <button v-on:click="toReportType(1)" id="reportSelection">Issues</button>
+      <button v-on:click="toReportType(2)" id="reportSelection">Recommendations</button>
     </div>
 
     <div class="issues">
+      <button id="pothole">Pothole</button>
+      <button id="sidewalks">Sidewalks</button>
+      <button id="vagrants">Vagrants</button>
+      <button id="other">Other</button>
       <!--Set of options based on report type-->
       <!--on click send data to server, mark as icon on map for temporary time (2 weeks?)-->
     </div>
     <div class="recommendations">
+      Duty
       <!--Set of options based on report type-->
       <!--on click send data to server, mark as icon on map for temporary time (2 weeks?)-->
     </div>
   </div>
 </template>
+<script type="text/javascript">
+  export default {
+    methods: {
+      toReportType: (reportType) => {
+        document.getElementsByClassName('reporting')[0].removeAttribute('id')
+        if (reportType === 1) {
+          document.getElementsByClassName('issues')[0].setAttribute('id', 'selected')
+        } else {
+          document.getElementsByClassName('recommendations')[0].setAttribute('id', 'selected')
+        }
+      }
+    }
+  }
+</script>
