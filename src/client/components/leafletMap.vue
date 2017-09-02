1f2dd2b3b0c760fe78c70b1286a6497b1aabe352
Demetrio <dmitr77@gmail.com>
2017-09-01 19:30:41 -0500

    linter fixes

--- src/client/components/leafletMap.vue
+++ src/client/components/leafletMap.vue
@@ -5,7 +5,12 @@
     <areaReporting></areaReporting>
 =======
     <Dropdown :toggleLayer="toggleLayer"></Dropdown>
+<<<<<<< HEAD
 >>>>>>> hamburger slide in added
+=======
+    <areaReporting></areaReporting>
+
+>>>>>>> linter fixes
   </div>
 </template>
 
@@ -102,38 +107,7 @@
         //end map creation
 
         //map location
-
-        let clientlng = navigator.geolocation.watchPosition((position) => position.coords.longitude )
-        let clientlat = navigator.geolocation.watchPosition((position) => position.coords.latitude )
-        let marker = L.marker([51.505, -0.09]).addTo(mymap);
-        marker.bindPopup('Configuring your location...').openPopup()
-        var circle = L.circle([51.505, -0.09], 0).addTo(mymap)
-
-
-        function onLocationFound(e) {
-          if (circle) {
-            mymap.removeLayer(circle)
-          }
-          var radius = e.accuracy / 2
-          var latln = {lat: e.latitude, lng: e.longitude}
-          mymap.setView(latln, 18)
-          marker.setLatLng(latln).closePopup()
-          .bindPopup("You are within " + radius + " meters from this point").openPopup()
-          circle = L.circle(latln, radius).addTo(mymap)
-        }
-        mymap.on('locationfound', onLocationFound)
-
-        if (navigator.geolocation) {
-          navigator.geolocation.watchPosition((position) => {
-            onLocationFound(position.coords)
-          })
-        }
-        mymap.locate()
-
-
-        //add to here later
-        //mLocation.locate(this, mymap)
-
+        mLocation.locate(this, mymap)
         //end map location
 
         // layer control
@@ -146,12 +120,16 @@
           }
         }
 <<<<<<< HEAD
+<<<<<<< HEAD
         function click (e) {
           console.log('One, ah ah ah');
         }
 =======
         
 >>>>>>> hamburger slide in added
+=======
+
+>>>>>>> linter fixes
         function doubleClick (e) {
           console.log('TWO, AH AH AH');
           let pos = [e.latlng.lat, e.latlng.lng]
@@ -173,5 +151,5 @@
 </script>
 
 <style>
-  #mapid {height: 500px;}
+  #mapid {height: 100%;}
 </style>
--- src/client/components/leafletMethods/methLocation.js
+++ src/client/components/leafletMethods/methLocation.js
@@ -1,18 +1,30 @@
 /* eslint-disable no-undef */
 export default {
   locate: (context, mymap) => {
+    // const clientlng = navigator.geolocation.watchPosition((position) => position.coords.longitude )
+    // const clientlat = navigator.geolocation.watchPosition((position) => position.coords.latitude )
     const marker = L.marker([51.505, -0.09]).addTo(mymap)
     marker.bindPopup('Configuring your location...').openPopup()
-
-    mymap.locate({ setView: true, zoom: 10 })
+    let circle = L.circle([51.505, -0.09], 0).addTo(mymap)
 
     function onLocationFound(e) {
+      if (circle) {
+        mymap.removeLayer(circle)
+      }
       const radius = e.accuracy / 2
-      L.marker(e.latlng)
-        .addTo(mymap)
-        .bindPopup(`You are within ${radius} meters from this point`).openPopup()
-      L.circle(e.latlng, radius).addTo(mymap)
+      const latln = {lat: e.latitude, lng: e.longitude}
+      mymap.setView(latln, 18)
+      marker.setLatLng(latln).closePopup()
+      .bindPopup("You are within " + radius + " meters from this point").openPopup()
+      circle = L.circle(latln, radius).addTo(mymap)
     }
     mymap.on('locationfound', onLocationFound)
+
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition((position) => {
+        onLocationFound(position.coords)
+      })
+    }
+    mymap.locate()
   }
 }
